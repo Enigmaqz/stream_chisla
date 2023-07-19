456d94a1379157848a7df8044e9dc9abf52501b9
Boffka92 <boffka92@ya.ru>
2023-07-19 22:24:07 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,20 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         List<Integer> positiveList = new ArrayList<>();
-        for(int i = 0; i < intList.size(); i++) {
+        for (int i = 0; i < intList.size(); i++) {
             if (intList.get(i) > 0) {
                 positiveList.add(intList.get(i));
             }
         }
         List<Integer> evenList = new ArrayList<>();
-        for(int i = 0; i < positiveList.size(); i++) {
+        for (int i = 0; i < positiveList.size(); i++) {
             if (positiveList.get(i) % 2 == 0) {
                 evenList.add(positiveList.get(i));
             }
         }
         Collections.sort(evenList);
-        System.out.println(evenList);
+        for (int i = 0; i < evenList.size(); i++) {
+            System.out.println(evenList.get(i));
+        }
     }
 }
